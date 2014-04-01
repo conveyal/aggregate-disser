02a9c7d0085ee81fd482c9c416343374e7338916
Brandon Martin-Anderson <badhill@gmail.com>
2014-04-01 15:21:52 -0700

    correlate two shapefiles

--- src/main/java/com/conveyal/disser/Disser.java
+++ src/main/java/com/conveyal/disser/Disser.java
@@ -2,117 +2,149 @@ package com.conveyal.disser;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.Random;
+import java.util.Map.Entry;
 
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
 import org.geotools.data.FeatureSource;
+import org.geotools.data.Query;
+import org.geotools.factory.CommonFactoryFinder;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
+import org.geotools.filter.Filter;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.opengis.feature.Feature;
 import org.opengis.feature.GeometryAttribute;
-import org.opengis.geometry.BoundingBox;
+import org.opengis.feature.type.FeatureType;
+import org.opengis.filter.FilterFactory2;
+import org.opengis.filter.spatial.BBOX;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
-import com.conveyal.disser.census.CensusRecord;
-import com.conveyal.disser.census.CensusTable;
-import com.conveyal.disser.census.SummaryFile;
 import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.MultiPolygon;
 import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 
 
 public class Disser {
     public static void main(String[] args) throws Exception {
-    	if( args.length < 6 ) {
-    		System.out.println( "usage: cmd shapfile basedir stateabbrev year table field" );
+    	if( args.length < 4 ) {
+    		System.out.println( "usage: cmd indicator_shp indicator_fld diss_shp diss_fld" );
     		return;
     	}
-    	
-    	String shapefile_name = args[0];
-    	String basedir = args[1];
-    	String stateabbrev = args[2];
-    	int year = Integer.parseInt(args[3]);
-    	String tableName = args[4];
-    	int field = Integer.parseInt(args[5]);
-    	
-    	// read a census table
-    	SummaryFile sf1 = new SummaryFile(basedir,stateabbrev,year);
+    	    	
+    	String indicator_shp = args[0];
+    	String indicator_fld = args[1];
+    	String diss_shp = args[2];
+    	String diss_fld = args[3];
       
-    	System.out.println( "reading sf1 geo table" );
-    	Map<String,String> eg = sf1.getGeoTable();
-    	System.out.println( "done, "+eg.size()+" records" );
-      
-    	System.out.println( "reading "+tableName+" table" );
-    	CensusTable table = sf1.getTable(tableName);
-    	System.out.println( "table has "+table.records.size()+" records");
-      
-    	//=================================
+    	//==== get indicator shapefile
     	
     	// construct shapefile factory
-        File file = new File( shapefile_name );
+        File file = new File( indicator_shp );
         Map<String,URL> map = new HashMap<String,URL>();
         map.put( "url", file.toURI().toURL() );
         DataStore dataStore = DataStoreFinder.getDataStore( map );
         
         // get shapefile as generic 'feature source'
         String typeName = dataStore.getTypeNames()[0];
-        FeatureSource<?, ?> source = dataStore.getFeatureSource( typeName );
+        FeatureSource<?, ?> indicatorSource = dataStore.getFeatureSource( typeName );
         
-        // get feature iterator from feature source
-        FeatureCollection<?, ?> collection = source.getFeatures( );
+        //==== get diss source
+        
+    	// construct shapefile factory
+        File dissFile = new File( diss_shp );
+        Map<String,URL> dissMap = new HashMap<String,URL>();
+        dissMap.put( "url", dissFile.toURI().toURL() );
+        DataStore dissDataStore = DataStoreFinder.getDataStore( dissMap );
+        
+        // get shapefile as generic 'feature source'
+        String dissTypeName = dissDataStore.getTypeNames()[0];
+        FeatureSource<?, ?> dissSource = dissDataStore.getFeatureSource( dissTypeName );
+        
+        //==== make sure both shapefiles have the same CRS
+        CoordinateReferenceSystem crs1 = indicatorSource.getSchema().getCoordinateReferenceSystem();
+        CoordinateReferenceSystem crs2 = dissSource.getSchema().getCoordinateReferenceSystem();
+        if( !crs1.equals(crs2) ){
+        	throw new Exception( "Coordinate systems don't match. "+crs1+"\n\n"+crs2 );
+        }        
+        
+        //==== loop through indicator shapefile, finding overlapping diss items
+    	HashMap<Geometry,ArrayList<Geometry>> indToDiss = new HashMap<Geometry,ArrayList<Geometry>>();
+    	HashMap<Geometry,ArrayList<Geometry>> dissToInd = new HashMap<Geometry,ArrayList<Geometry>>();
+        
+        FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2();
+        FeatureType schema = dissSource.getSchema();
+        String geometryPropertyName = schema.getGeometryDescriptor().getLocalName();
+        CoordinateReferenceSystem dissCRS = schema.getGeometryDescriptor().getCoordinateReferenceSystem();
+        
+        // get the part of the indicator file where diss items will be found
+        ReferencedEnvelope dissBBox = dissSource.getBounds();
+        String indicatorPropertyName = indicatorSource.getSchema().getGeometryDescriptor().getLocalName();
+        BBOX dissFilter = ff.bbox(ff.property(indicatorPropertyName), dissBBox);
+
+        FeatureCollection<?, ?> collection = indicatorSource.getFeatures(dissFilter);
         FeatureIterator<?> iterator = collection.features();
+        int n = collection.size();
         
-        // print out disaggregated points
-        int n=0;
+        int i=0;
         while( iterator.hasNext() ){
-             Feature feature = (Feature) iterator.next();
-             GeometryAttribute geo = feature.getDefaultGeometryProperty();
-             
-             String geoid10 = (String)feature.getProperties("GEOID10").iterator().next().getValue();
-             String logrecno = eg.get(geoid10);
-             CensusRecord rec = table.getRecord( logrecno );
-             int count = rec.getCensusIntField( field );
-             
-             n += count;
-
-         	 for(int i=0; i<count; i++){
-         		 Point rando = getRandomPoint( geo );
-                 System.out.println( rando.getX()+","+rando.getY()+",1" );
-                 n++;
-         	 }
-        }
-        System.out.println( "done with "+n+" points" );
-    }
-
-	private static Point getRandomPoint(GeometryAttribute geo) throws Exception {
-        if( !geo.getType().getName().toString().equals("MultiPolygon") ){
-       	  throw new Exception( "Geometry isn't a multipolygon; don't know what to do." );
-        }
-        
-        BoundingBox bb = geo.getBounds();
-        
-        MultiPolygon mp = (MultiPolygon)geo.getValue();
-        
-        Random rand = new Random();
-        GeometryFactory gf = new GeometryFactory();
-        Point pt = null;
-        for(int i=0; i<1000; i++){
-        	double x = randdouble(rand, bb.getMinX(),bb.getMaxX());
-        	double y = randdouble(rand, bb.getMinY(),bb.getMaxY());
-        	pt = gf.createPoint( new Coordinate(x,y) );
-        	if(mp.contains(pt)){
-        		break;
+        	if(i%100==0){
+        		System.out.println( i+"/"+n+" ("+(100*(float)i)/n+"%)" );
         	}
+        	
+             Feature feature = (Feature) iterator.next();
+             GeometryAttribute geoAttr = feature.getDefaultGeometryProperty();
+             Geometry indGeo = (Geometry)geoAttr.getValue();
+             
+//             //int indicator_val = (Integer)feature.getProperties(indicator_fld).iterator().next().getValue();
+             
+             // Get every diss geometry that intersects with the indicator geometry           
+             ReferencedEnvelope bbox = new ReferencedEnvelope(dissCRS);
+             bbox.setBounds(geoAttr.getBounds());
+             BBOX filter = ff.bbox(ff.property(geometryPropertyName), bbox);
+             FeatureCollection<?, ?> dissCollection = dissSource.getFeatures(filter);
+             FeatureIterator<?> dissIterator = dissCollection.features();
+             int overlapN=0;
+             while(dissIterator.hasNext()){
+            	 Feature diss = (Feature)dissIterator.next();
+            	 GeometryAttribute dissGeoAttr = diss.getDefaultGeometryProperty();
+            	 Geometry dissGeo = (Geometry)dissGeoAttr.getValue();
+            	 
+            	 if(dissGeo.intersects(indGeo)){
+            		 ArrayList<Geometry> inds = dissToInd.get(dissGeo);
+            		 if(inds==null){
+            			 inds = new ArrayList<Geometry>();
+            			 dissToInd.put(dissGeo, inds);
+            		 }
+            		 inds.add(indGeo);
+            		 
+            		 ArrayList<Geometry> disss = indToDiss.get(indGeo);
+            		 if(disss==null){
+            			 disss = new ArrayList<Geometry>();
+            			 indToDiss.put(indGeo, disss);
+            		 }
+            		 disss.add(indGeo);
+            		 
+            		 overlapN++;
+            	 }
+             }
+             dissIterator.close();
+             //System.out.println( "indicator geo "+i+" bbox overlaps "+dissCollection.size()+", true overlap "+overlapN+" items" );
+//             
+             i++;
         }
         
-        return pt;
-	}
-
-	private static double randdouble(Random rand, double minX, double maxX) {
-		return minX + rand.nextDouble()*(maxX-minX);
-	}
+        for( Entry<Geometry, ArrayList<Geometry>> entry : dissToInd.entrySet() ){
+        	Geometry dissGeo = entry.getKey();
+        	ArrayList<Geometry> indGeos = entry.getValue();
+        	System.out.println( "diss geom has "+indGeos.size()+" ind geoms" );
+        }
+    }
 }
